c06beae47fc8e4bb05aacf33c48e0595aef682a0
Zrouga Mohamed <61499067+zrougamed@users.noreply.github.com>
2021-06-14 23:38:23 +0100

    adding ajax handler

--- index.py
+++ index.py
@@ -2,6 +2,17 @@ from flask import Flask,render_template,request
 
 
 app = Flask(__name__)
+START = 0
+def dorker(keySearch,keyMail,keySites):
+   
+   CX_ID = "6de800d51f748f99f"
+   API_KEY = "AIzaSyALJZaTRZBbQzwBI21da1d9qgSVpim28sQ"
+
+   STR_SEARCH = '"{}"++"{}"'.format(keySearch)
+   LINKED_IN = '+-intitle:"profiles"+-inurl:"dir/+"+site:www.linkedin.com/in/+OR+site:www.linkedin.com/pub/'
+   url = "https://www.googleapis.com/customsearch/v1?key={}&cx={}&start={}&q={}"
+
+
 
 @app.route("/",methods=["GET","POST"])
 def index():
